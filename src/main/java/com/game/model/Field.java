ba18300ce743229b9c62cb288a6d95c757402023
Serhii Malykhin <serhii_malykhin@epam.com>
2018-01-23 15:58:15 +0200

    [refactoring] added toString to Field

--- src/main/java/com/game/model/Field.java
+++ src/main/java/com/game/model/Field.java
@@ -5,12 +5,26 @@ public class Field {
 
   public Field(Cell[][] cells) {
     this.cells = cells;
-    if(cells.length != cells[0].length){
+    if (cells.length != cells[0].length) {
       throw new IllegalStateException("Field must be NxN");
     }
   }
 
   public Cell[][] getCells() {
     return cells;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+
+    for (int i = 0; i < cells.length; i++) {
+      for (int j = 0; j < cells.length; j++) {
+        builder.append(cells[i][j].getCellValue())
+                .append(' ');
+      }
+      builder.append('\n');
+    }
+    return builder.toString();
   }
 }
